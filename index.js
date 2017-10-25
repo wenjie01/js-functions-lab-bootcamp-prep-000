630da64d34d62993735cd653675c821252625849
Wenjie <xuwenjie8866@gmail.com>
2017-10-25 18:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
+function holidayCountdown(days, holiday) {
+  return `It's ${day} days until ${holiday}!`;
+}
